43d58513d97a2d66eb1d4b79e34c581dd75e4a04
Jhon Sisneros <jhonsisneros99@gmail.com>
2023-05-17 11:04:58 -0400

    finished adding header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="--header">
+      <img src="/images/troll-face.png" className="--header-logo"></img>
+      <h1 className="--header-text">Meme Generator</h1>
+      <h3 className="--header-subText">Make your best meme!</h3>
+    </div>
+  );
+};
+export default Header;
